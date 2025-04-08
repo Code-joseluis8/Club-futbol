cc2334324b5d1273b06ae8f6f75d212440b3000f
unknown <joseluisquintho@gmail.com>
2025-04-07 19:37:27 -0500

    Cambios En Los Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "../../Club-futbol/src/app/components/home/home.component";
+import { EquiposComponent } from "../../Club-futbol/src/app/components/equipos/equipos.component";
+import { LoginComponent } from "../../Club-futbol/src/app/components/login/login.component";
+import { LugarJogoComponent } from "../../Club-futbol/src/app/components/lugar-jogo/lugar-jogo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, EquiposComponent, LoginComponent, LugarJogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Club-futbol';
+  
+  
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../../Club-futbol/src/app/components/home/home.component';
+import { EquiposComponent } from '../../Club-futbol/src/app/components/equipos/equipos.component';
+import { LoginComponent } from '../../Club-futbol/src/app/components/login/login.component';
+import { LugarJogoComponent } from '../../Club-futbol/src/app/components/lugar-jogo/lugar-jogo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path: 'equipos',
+      component: EquiposComponent,
+
+   },
+   {
+      path: 'home',
+      component: HomeComponent,
+   },
+
+   {
+      path: 'login',
+      component: LoginComponent,
+   },
+   {
+      path: 'lugar-jogo',
+      component: LugarJogoComponent,
+   }
+   
+];
+
+
+
